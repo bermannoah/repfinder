c7a1f9a77cfd0bdbf0a6a2b768050584e303d6a4
Noah Berman <noah@noahberman.org>
2017-02-08 09:45:05 -0700

    parser correct stores the data

--- parser.rb
+++ parser.rb
@@ -1,5 +1,5 @@
-require 'congress'
 require 'csv'
+require 'bunny'
 require 'pry'
 
 class Parser
@@ -8,8 +8,16 @@ class Parser
   def read_csv(path_to_csv)
     @all_rows_array = []
     CSV.foreach(path_to_csv) do |row|
-      @all_rows_array.push(row)
+      student_hash = {}
+      student_hash[:first_name] = row[2]
+      student_hash[:last_name] = row[3]
+      student_hash[:street] = row[6]
+      student_hash[:city] = row[7]
+      student_hash[:state] = row[8]
+      student_hash[:zipcode] = row[9]
+      @all_rows_array << student_hash
     end
+    @all_rows_array.shift
     @all_rows_array
   end
 end
